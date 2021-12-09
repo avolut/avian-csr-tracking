dc4b99483a2ae9566dab8859d270ee7e4d81241b
rpb99 <rikipebrianto@gmail.com>
2021-12-09 12:45:11 +0700

    fix

--- app/web/src/base/page/50012-home.base.tsx
+++ app/web/src/base/page/50012-home.base.tsx
@@ -9,9 +9,9 @@ base(
         <div class="flex flex-1 self-stretch items-start justify-start">
           <div class="flex flex-1 self-stretch flex-col items-start justify-start bg-white">
             <div class={`flex self-stretch flex-col items-start justify-start`}>
-              <w-topbar
+              {/* <w-topbar
                 class={`flex flex-1 self-stretch items-center justify-start px-5 bg-green-50 border border-indigo-50`}
-              />
+              /> */}
             </div>
           </div>
         </div>
