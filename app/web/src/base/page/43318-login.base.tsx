db507b54378429770d32c22a39874eb1d231f723
nizamiftahul <nizamiftahul@gmail.com>
2021-12-10 13:05:53 +0700

    fixing

--- app/web/src/base/page/43318-login.base.tsx
+++ app/web/src/base/page/43318-login.base.tsx
@@ -3,9 +3,10 @@ base(
     meta: {
       username: '',
       password: '',
-      action: {},
+      action: {} as any,
+      isMobile: window.innerWidth <= 768,
     },
-    init: async ({ meta }) => {
+    init: ({ meta }) => {
       runInAction(() => {
         meta.action = {
           onChange: (e, field) => {
@@ -41,12 +42,93 @@ base(
             }
           },
         }
+
+        const handleWindowSizeChange = action(
+          () => (meta.isMobile = window.innerWidth <= 768)
+        )
+        window.addEventListener('resize', handleWindowSizeChange)
+        return () => {
+          window.removeEventListener('resize', handleWindowSizeChange)
+        }
       })
     },
   },
   ({ meta }) => (
     <>
-      <>
+      {meta.isMobile ? (
+        <div
+          className="flex flex-1 h-screen items-center"
+          style={css`
+            background: url('/fimgs/472_4252.x1.png');
+          `}
+        >
+          <div
+            className="block flex-1 m-4 p-4"
+            style={css`
+              background: rgba(255, 255, 255, 0.7);
+              height: 300px;
+            `}
+          >
+            <div
+              class={`flex self-stretch flex-col items-center justify-center`}
+            >
+              <div class="text-xs">WELCOME TO</div>
+              <div class="font-bold text-2xl text-green-800">CSR TRACKING</div>
+            </div>
+            <form
+              class={`flex flex-col items-center justify-center`}
+              onSubmit={(e) => {
+                e.preventDefault()
+                meta.action.login()
+              }}
+            >
+              <div class={`flex flex-col gap-4 self-stretch items-end justify-start mt-5 p-2.5`}>
+                <input
+                  type={'text'}
+                  class={`bg-white`}
+                  placeholder="Username"
+                  value={meta.username}
+                  onChange={(e) => meta.action.onChange(e, 'username')}
+                  style={css`
+                    width: 100%;
+                    border: 1px solid #cececb !important;
+                    background: white !important;
+                    border-radius: 15px !important;
+                    padding: 5px !important;
+                    color: #727272 !important;
+                  `}
+                />
+                <input
+                  class={`flex flex-col space-y-2.5 items-start justify-start w-full border-b`}
+                  type="password"
+                  placeholder="Password"
+                  type="password"
+                  value={meta.password}
+                  onChange={(e) => meta.action.onChange(e, 'password')}
+                  style={css`
+                    width: 100%;
+                    border: 1px solid #cececb !important;
+                    background: white !important;
+                    border-radius: 15px !important;
+                    padding: 5px !important;
+                    color: #727272 !important;
+                  `}
+                />
+              </div>
+              <div
+                class={`flex self-stretch flex-col items-center justify-end px-2.5 pt-5 pb-2.5`}
+              >
+                <btn
+                  class={`flex items-center justify-center px-4 py-2 bg-green-600 rounded w-full text-white rounded-full`}
+                  type={'submit'}
+                >
+                  Masuk
+                </btn>
+              </div>
+            </form>
+          </div>
+        </div>
+      ) : (
         <div class={`flex flex-1 self-stretch items-start justify-start`}>
           <img
             src="/fimgs/472_4252.x1.png"
@@ -112,7 +194,7 @@ base(
             </div>
           </div>
         </div>
-      </>
+      )}
     </>
   )
 )
